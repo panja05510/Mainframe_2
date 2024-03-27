bfe88737927f449b9a166677d48a652e68eb032f
goelbhrat02 <goelbhrat02.bg@gmail.com>
2024-03-27 21:28:28 +0530

    change schema

--- Mainframe2/src/main/java/com/citizens/mainframe/model/Accounts.java
+++ Mainframe2/src/main/java/com/citizens/mainframe/model/Accounts.java
@@ -10,28 +10,21 @@ public class Accounts {
 	private String id;
 	private String serviceName;
 	private Integer delay;
+	private Map<String, String> response;
 
-	public Accounts(String id, String serviceName, Integer delay, Map<Integer, String> response) {
+	@Override
+	public String toString() {
+		return "Accounts [id=" + id + ", serviceName=" + serviceName + ", delay=" + delay + ", response=" + response
+				+ "]";
+	}
+
+	public Accounts(String id, String serviceName, Integer delay, Map<String, String> response) {
 		super();
 		this.id = id;
 		this.serviceName = serviceName;
 		this.delay = delay;
 		this.response = response;
 	}
-
-	public Integer getDelay() {
-		return delay;
-	}
-
-	public void setDelay(Integer delay) {
-		this.delay = delay;
-	}
-
-	private Map<Integer, String> response;
-	/*
-	 * "90982": "some error fixed lenght response 01",
-	 * "87348":"some error fixed lenght resonse 02 "
-	 */
 
 	public String getId() {
 		return id;
@@ -49,20 +42,20 @@ public class Accounts {
 		this.serviceName = serviceName;
 	}
 
-	public Map<Integer, String> getResponse() {
+	public Integer getDelay() {
+		return delay;
+	}
+
+	public void setDelay(Integer delay) {
+		this.delay = delay;
+	}
+
+	public Map<String, String> getResponse() {
 		return response;
 	}
 
-	public void setResponse(Map<Integer, String> response) {
+	public void setResponse(Map<String, String> response) {
 		this.response = response;
-	}
-
-	
-	
-	@Override
-	public String toString() {
-		return "Accounts [id=" + id + ", serviceName=" + serviceName + ", delay=" + delay + ", response=" + response
-				+ "]";
 	}
 
 }
